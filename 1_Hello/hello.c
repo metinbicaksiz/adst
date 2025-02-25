02f61b4632d0fde1ba6cdc4fb86e8e6dfe81db74
Metin Bicaksiz <72347095+metinbicaksiz@users.noreply.github.com>
2025-02-25 19:19:50 +0000

    type conversion

--- 1_Hello/hello.c
+++ 1_Hello/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
+    // printf("Hello world!\n");
+
+    int scores = 574;
+    int sts = 15;
+
+    float sum = (float) scores / sts;
+
+    printf("my result is: %.2f \n", sum);
+
     return 0;
 }
