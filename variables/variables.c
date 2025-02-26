bfedf46d0f7d48458cd7b9fbcf7357aca43168dc
metinbicaksiz <metinbicaksiz@gmail.com>
2025-02-26 11:18:45 -0800

    variable naming

--- variables/variables.c
+++ variables/variables.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int main() {
+//
+//    int studentID = 15;
+//    int studentAge = 23;
+//    float studentFee = 75.25;
+//    char studentGrade = 'B';
+//
+//        // Print variables
+//    printf("Student id: %d\n", studentID);
+//    printf("Student age: %d\n", studentAge);
+//    printf("Student fee: %.2f\n", studentFee);
+//    printf("Student grade: %c \n", studentGrade);
 
-  int myNum = 10;
-  int numTwo;
+//    float length = 6.7;
+//    int width = 4;
+//
+//    float area = length * width;
+//
+//    printf("The area is: %.2f \n", area);
 
-  numTwo = myNum;
+    int myMod = 1;
 
-  printf("my result is: %d\n", numTwo);
+//    myMod++;
+//    myMod--;
+    printf("my modulus is: %d \n", myMod);
 
-  return 0;
+    return 0;
 }
