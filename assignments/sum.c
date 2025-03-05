0710c1056b1d75991b730ca78c350d9e68064e7e
hasleenkaur <h.kaur@gobindsarvar.ca>
2025-03-05 19:06:11 +0000

    Second Assignment: Sum, Multiplication & Division

--- assignments/sum.c
+++ assignments/sum.c
@@ -9,7 +9,21 @@ int main() {
     Expected Output:
 Sum of the above two integers = 63
      */
+    int num1 = 25;
+    int num2 = 58;
+    int sum;
 
+    printf("The first number is: %d\n", num1);
+    printf("The second number is: %d\n", num2);
+
+    sum = num1 + num2;
+    printf ("The sum is: %d\n", sum);
+
+    int multiply = num1 *num2;
+    float divide = (float)num1 / num2;
+
+    printf ("The multiplication is: %d\n", multiply);
+    printf ("The division is: %.2f\n", divide);
 
     return 0;
 }
