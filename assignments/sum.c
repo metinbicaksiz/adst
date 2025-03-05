b066f1c43db8a02e15d4a84892422e3e10b22e7a
metinbicaksiz <metinbicaksiz@gmail.com>
2025-03-05 10:37:13 -0800

    sum function tested

--- assignments/sum.c
+++ assignments/sum.c
@@ -7,9 +7,17 @@ int main() {
     Input the first integer: 25
     Input the second integer: 38
     Expected Output:
-Sum of the above two integers = 63
+    Sum of the above two integers = 63
      */
+    int num1 = 25;
+    int num2 = 50;
+    int sum;
 
+    printf("The first number is: %d\n", num1);
+    printf("The second number is: %d\n", num2);
+
+    sum = num1 + num2;
+    printf("The sum is: %d\n", sum);
 
     return 0;
 }
