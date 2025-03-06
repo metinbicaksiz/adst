6f14fdb2f962dcd25d703589e55b29a490850e94
hasleenkaur <h.kaur@gobindsarvar.ca>
2025-03-06 19:22:58 +0000

    fifth assignment (conditions)

--- assignments/conditions.c
+++ assignments/conditions.c
@@ -11,6 +11,13 @@ int main() {
     Please use if conditions.
      */
 
+    int age = 15;
+
+    if(age >= 18) {
+        printf("Congratulation! You are eligible for casting your vote.\n");
+    } else {
+        printf("Sorry! You are not eligible for casting your vote.\n");
+    }
 
     return 0;
 }
