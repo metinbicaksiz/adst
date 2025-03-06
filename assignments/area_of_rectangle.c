532deb26ebaadedbed75513d6e00ace3b1771164
Gurleen <kaur.gurleen@gobindsarvar.ca>
2025-03-06 18:45:15 +0000

    3rd work area

--- assignments/area_of_rectangle.c
+++ assignments/area_of_rectangle.c
@@ -9,6 +9,15 @@ int main() {
       Area of the rectangle = 35 square inches
 
    */
+    int height = 7 , width = 5;
+    int perimeter,area;
+
+    perimeter = 2 * (height + width);
+    area = height * width;
+
+    printf("Perimeter of the rectangle = %d inches\n", perimeter);
+    printf("Area of the rectangle      = %d square inches\n", area);
+
 
   return 0;
 }
