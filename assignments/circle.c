56d43ebe623ba1921fbdf9c4b718d5a025da1627
amaanatkaur <a.randhawa@gobindsarvar.ca>
2025-03-07 19:18:30 +0000

    Area of Circle

--- assignments/circle.c
+++ assignments/circle.c
@@ -8,6 +8,11 @@ int main() {
         Perimeter of the Circle = 37.680000 inches
         Area of the Circle = 113.040001 square inches
      */
-
+     float radius = 5;
+     float PI = 3.1416;
+     float perimeter = 2 * PI * radius;
+     float area = PI * radius * radius;
+     printf("Perimeter of the circle = %.2f inches\n", perimeter);
+     printf("Area of the circle = %.2f square inches\n", area);
     return 0;
 }
